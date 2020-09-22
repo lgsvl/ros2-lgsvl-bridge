03bf0888058f7068fc41b725e654769802218207
Guodong Rong <guodong.rong@lge.com>
2020-09-22 09:42:48 -0700

    Use two buffers to handle message larger than 65536 bytes.

--- src/client.cc
+++ src/client.cc
@@ -6,83 +6,64 @@
  */
 
 #include "client.h"
-#include "clients.h"
-#include "node.h"
-#include "logging.h"
 
+#include <boost/bind.hpp>
+#include <functional>
 #include <memory>
 #include <string>
-#include <functional>
-#include <boost/bind.hpp>
 
-enum
-{
+#include "clients.h"
+#include "logging.h"
+#include "node.h"
+
+enum {
     OP_ADD_SUBSCRIBER = 1,
-    OP_ADD_PUBLISHER  = 2,
-    OP_PUBLISH        = 3,
+    OP_ADD_PUBLISHER = 2,
+    OP_PUBLISH = 3,
 };
 
 Client::Client(Node& node, Clients& clients, boost::asio::ip::tcp::socket s)
-    : node(node)
-    , clients(clients)
-    , socket(std::move(s))
-{
+    : node(node), clients(clients), socket(std::move(s)) {
     auto endpoint = socket.remote_endpoint();
-    LOG("Client [" << endpoint.address() << ":" << endpoint.port() << "] connected");
+    LOG("Client [" << endpoint.address() << ":" << endpoint.port()
+                   << "] connected");
 }
 
-Client::~Client()
-{
-}
+Client::~Client() {}
 
-void Client::start()
-{
+void Client::start() {
     socket.async_read_some(
         boost::asio::buffer(temp, sizeof(temp)),
-        boost::bind(
-            &Client::handle_read,
-            shared_from_this(),
-            boost::asio::placeholders::error,
-            boost::asio::placeholders::bytes_transferred));
+        boost::bind(&Client::handle_read, shared_from_this(),
+                    boost::asio::placeholders::error,
+                    boost::asio::placeholders::bytes_transferred));
 }
 
-void Client::stop()
-{
-    socket.close();
-}
+void Client::stop() { socket.close(); }
 
-void Client::handle_read(const boost::system::error_code& ec, std::size_t size)
-{
-    if (!ec)
-    {
+void Client::handle_read(const boost::system::error_code& ec,
+                         std::size_t size) {
+    if (!ec) {
         DEBUG("Received " << size << " bytes");
         buffer.insert(buffer.end(), temp, temp + size);
 
         size_t size = buffer.size();
 
-        while (size >= sizeof(uint8_t))
-        {
-            if (buffer[0] == OP_ADD_SUBSCRIBER)
-            {
+        while (size >= sizeof(uint8_t)) {
+            if (buffer[0] == OP_ADD_SUBSCRIBER) {
                 handle_add_subscriber();
-            }
-            else if (buffer[0] == OP_ADD_PUBLISHER)
-            {
+            } else if (buffer[0] == OP_ADD_PUBLISHER) {
                 handle_add_publisher();
-            }
-            else if (buffer[0] == OP_PUBLISH)
-            {
+            } else if (buffer[0] == OP_PUBLISH) {
                 handle_publish();
-            }
-            else
-            {
-                ERROR("Unknown operation received from client (" << uint32_t(buffer[0]) << "), disconnecting client");
+            } else {
+                ERROR("Unknown operation received from client ("
+                      << uint32_t(buffer[0]) << "), disconnecting client");
                 clients.stop(shared_from_this());
                 return;
             }
 
-            if (size == buffer.size())
-            {
+            if (size == buffer.size()) {
                 break;
             }
             size = buffer.size();
@@ -92,8 +73,7 @@ void Client::handle_read(const boost::system::error_code& ec, std::size_t size)
         return;
     }
 
-    if (ec == boost::asio::error::eof)
-    {
+    if (ec == boost::asio::error::eof) {
         // remote side closed connection
         LOG("Client disconnected");
         clients.stop(shared_from_this());
@@ -101,8 +81,7 @@ void Client::handle_read(const boost::system::error_code& ec, std::size_t size)
         return;
     }
 
-    if (ec != boost::asio::error::operation_aborted)
-    {
+    if (ec != boost::asio::error::operation_aborted) {
         ERROR("Client read failed, disconnecting: " << ec.message());
         clients.stop(shared_from_this());
         node.remove(shared_from_this());
@@ -110,22 +89,30 @@ void Client::handle_read(const boost::system::error_code& ec, std::size_t size)
     }
 }
 
-void Client::handle_write(const boost::system::error_code& ec)
-{
-    if (ec && ec != boost::asio::error::operation_aborted)
-    {
-        ERROR("Client write failed, disconnecting: " << ec.message());
-        clients.stop(shared_from_this());
-        node.remove(shared_from_this());
+void Client::handle_write(const boost::system::error_code& ec) {
+    if (ec) {
+        if (ec != boost::asio::error::operation_aborted) {
+            ERROR("Client write failed, disconnecting" << ec.message());
+            clients.stop(shared_from_this());
+            node.remove(shared_from_this());
+        }
         return;
+    }
+
+    std::lock_guard<std::mutex> lock(publish_mutex);
+    writing.clear();
+    if (!pending.empty()) {
+        writing.swap(pending);
+        boost::asio::async_write(
+            socket, boost::asio::buffer(writing.data(), writing.size()),
+            boost::bind(&Client::handle_write, shared_from_this(),
+                        boost::asio::placeholders::error));
     }
 }
 
 // [OP_ADD_SUBSCRIBER] [topic] [type]
-void Client::handle_add_subscriber()
-{
-    if (sizeof(uint8_t) + 2 * sizeof(uint32_t) > buffer.size())
-    {
+void Client::handle_add_subscriber() {
+    if (sizeof(uint8_t) + 2 * sizeof(uint32_t) > buffer.size()) {
         DEBUG("handle_add_subscriber too short header");
         return;
     }
@@ -134,9 +121,9 @@ void Client::handle_add_subscriber()
 
     uint32_t topic_length = get32le(offset);
     offset += sizeof(uint32_t);
-    if (offset + topic_length > buffer.size())
-    {
-        DEBUG("handle_add_subscriber short1 " << (offset + topic_length) << " " << buffer.size());
+    if (offset + topic_length > buffer.size()) {
+        DEBUG("handle_add_subscriber short1 " << (offset + topic_length) << " "
+                                              << buffer.size());
         return;
     }
 
@@ -145,9 +132,9 @@ void Client::handle_add_subscriber()
 
     uint32_t type_length = get32le(offset);
     offset += sizeof(uint32_t);
-    if (offset + type_length > buffer.size())
-    {
-        DEBUG("handle_add_subscriber short2 " << (offset + type_length) << " " << buffer.size());
+    if (offset + type_length > buffer.size()) {
+        DEBUG("handle_add_subscriber short2 " << (offset + type_length) << " "
+                                              << buffer.size());
         return;
     }
 
@@ -162,10 +149,8 @@ void Client::handle_add_subscriber()
 }
 
 // [OP_ADD_PUBLISHER] [topic] [type]
-void Client::handle_add_publisher()
-{
-    if (sizeof(uint8_t) + 2 * sizeof(uint32_t) > buffer.size())
-    {
+void Client::handle_add_publisher() {
+    if (sizeof(uint8_t) + 2 * sizeof(uint32_t) > buffer.size()) {
         DEBUG("handle_add_publisher too short header");
         return;
     }
@@ -174,9 +159,9 @@ void Client::handle_add_publisher()
 
     uint32_t topic_length = get32le(offset);
     offset += sizeof(uint32_t);
-    if (offset + topic_length > buffer.size())
-    {
-        DEBUG("handle_add_publisher short1 " << (offset + topic_length) << " " << buffer.size());
+    if (offset + topic_length > buffer.size()) {
+        DEBUG("handle_add_publisher short1 " << (offset + topic_length) << " "
+                                             << buffer.size());
         return;
     }
 
@@ -185,9 +170,9 @@ void Client::handle_add_publisher()
 
     uint32_t type_length = get32le(offset);
     offset += sizeof(uint32_t);
-    if (offset + type_length > buffer.size())
-    {
-        DEBUG("handle_add_publisher short2 " << (offset + type_length) << " " << buffer.size());
+    if (offset + type_length > buffer.size()) {
+        DEBUG("handle_add_publisher short2 " << (offset + type_length) << " "
+                                             << buffer.size());
         return;
     }
 
@@ -202,10 +187,8 @@ void Client::handle_add_publisher()
 }
 
 // [OP_PUBLISH] [topic] [message]
-void Client::handle_publish()
-{
-    if (sizeof(uint8_t) + 2 * sizeof(uint32_t) > buffer.size())
-    {
+void Client::handle_publish() {
+    if (sizeof(uint8_t) + 2 * sizeof(uint32_t) > buffer.size()) {
         return;
     }
 
@@ -213,8 +196,7 @@ void Client::handle_publish()
 
     uint32_t topic_length = get32le(offset);
     offset += sizeof(uint32_t);
-    if (offset + topic_length > buffer.size())
-    {
+    if (offset + topic_length > buffer.size()) {
         return;
     }
 
@@ -223,12 +205,12 @@ void Client::handle_publish()
 
     uint32_t message_length = get32le(offset);
     offset += sizeof(uint32_t);
-    if (offset + message_length > buffer.size())
-    {
+    if (offset + message_length > buffer.size()) {
         return;
     }
 
-    std::vector<uint8_t> message(&buffer[offset], &buffer[offset] + message_length);
+    std::vector<uint8_t> message(&buffer[offset],
+                                 &buffer[offset] + message_length);
     offset += message_length;
 
     DEBUG("OP_PUBLISH, topic = " << topic);
@@ -238,38 +220,45 @@ void Client::handle_publish()
     buffer.erase(buffer.begin(), buffer.begin() + offset);
 }
 
-void Client::publish(const std::string& topic, const std::vector<uint8_t>& msg)
-{
-    std::vector<uint8_t> data;
-    data.reserve(sizeof(uint8_t) + sizeof(uint32_t) + topic.size() + sizeof(uint32_t) + msg.size());
+void fill_data(std::vector<uint8_t>* data, const std::string& topic,
+               const std::vector<uint8_t>& msg) {
+    data->reserve(data->size() + sizeof(uint8_t) + sizeof(uint32_t) +
+                  topic.size() + sizeof(uint32_t) + msg.size());
 
-    data.push_back(OP_PUBLISH);
+    data->push_back(OP_PUBLISH);
 
-    data.push_back(uint8_t(topic.size() >> 0));
-    data.push_back(uint8_t(topic.size() >> 8));
-    data.push_back(uint8_t(topic.size() >> 16));
-    data.push_back(uint8_t(topic.size() >> 24));
-    data.insert(data.end(), (uint8_t*)topic.data(), (uint8_t*)topic.data() + topic.size());
+    data->push_back(uint8_t(topic.size() >> 0));
+    data->push_back(uint8_t(topic.size() >> 8));
+    data->push_back(uint8_t(topic.size() >> 16));
+    data->push_back(uint8_t(topic.size() >> 24));
+    const uint8_t* topic_data = reinterpret_cast<const uint8_t*>(topic.data());
+    data->insert(data->end(), topic_data, topic_data + topic.size());
 
-    data.push_back(uint8_t(msg.size() >> 0));
-    data.push_back(uint8_t(msg.size() >> 8));
-    data.push_back(uint8_t(msg.size() >> 16));
-    data.push_back(uint8_t(msg.size() >> 24));
-    data.insert(data.end(), msg.data(), msg.data() + msg.size());
-
-    boost::asio::async_write(
-        socket,
-        boost::asio::buffer(data.data(), data.size()),
-        boost::bind(
-            &Client::handle_write,
-            shared_from_this(),
-            boost::asio::placeholders::error));
+    data->push_back(uint8_t(msg.size() >> 0));
+    data->push_back(uint8_t(msg.size() >> 8));
+    data->push_back(uint8_t(msg.size() >> 16));
+    data->push_back(uint8_t(msg.size() >> 24));
+    data->insert(data->end(), msg.data(), msg.data() + msg.size());
 }
 
-uint32_t Client::get32le(size_t offset) const
-{
-    return buffer[offset + 0]
-        | (buffer[offset + 1] << 8)
-        | (buffer[offset + 2] << 16)
-        | (buffer[offset + 3] << 24);
+void Client::publish(const std::string& topic,
+                     const std::vector<uint8_t>& msg) {
+    std::lock_guard<std::mutex> lock(publish_mutex);
+    if (writing.empty()) {
+        fill_data(&writing, topic, msg);
+        boost::asio::async_write(
+            socket, boost::asio::buffer(writing.data(), writing.size()),
+            boost::bind(&Client::handle_write, shared_from_this(),
+                        boost::asio::placeholders::error));
+    } else if (pending.size() < MAX_PENDING_SIZE) {
+        fill_data(&pending, topic, msg);
+    } else {
+        // If pending size is larger than MAX_PENDING_SIZE, discard the message.
+        ERROR("Pending size too large. Discard message.");
+    }
+}
+
+uint32_t Client::get32le(size_t offset) const {
+    return buffer[offset + 0] | (buffer[offset + 1] << 8) |
+           (buffer[offset + 2] << 16) | (buffer[offset + 3] << 24);
 }
